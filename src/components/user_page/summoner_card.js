8fd26b893bfd06a5fc96ea6bc5b76b3d646be662
jacksonschuler <jacksonschuler11@gmail.com>
2020-11-02 14:19:59 -0600

    summoner_card correctly loading data from league api, needs feedback while component is loading

--- src/components/user_page/summoner_card.js
+++ src/components/user_page/summoner_card.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import {makeStyles} from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import Divider from "@material-ui/core/Divider";
 import Avatar from "@material-ui/core/Avatar";
 
 
 const useStyles = makeStyles((theme) => ({
     card: {
         borderRadius: 35,
-        minWidth: 325,
-        maxWidth: 325,
-        minHeight: 460,
+        minWidth: 275,
+        maxWidth: 275,
+        minHeight: 420, //nice
         boxShadow: '0 16px 40px -12.125px rgba(0,0,0,0.3)',
+        backgroundColor: '#fff'
     },
     card_wave_svg: {
         position: 'relative',
         display: 'block',
         width: 'calc(100% + 1.3px)',
-        height: 90
+        height: 91
     },
     name: {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        marginBottom: -35
+        marginBottom: -35,
+        fontFamily: 'Quicksand',
     },
     avatar_container: {
         display: 'flex',
@@ -34,6 +35,30 @@ const useStyles = makeStyles((theme) => ({
         height: 30,
         width: 30,
         marginRight: theme.spacing(1)
+    },
+    img : {
+        width:200,
+        height: 200,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        borderRadius: 35,
+        borderStyle: 'solid',
+        borderColor: '#fff',
+        borderWidth: '5px',
+        zIndex: 2,
+        boxShadow: '0 16px 40px -12.125px rgba(0,0,0,0.3)',
+    },
+    img_container: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: -225,
+        marginBottom: 15
+    },
+    divider: {
+        marginTop: 30,
+        marginLeft: 40,
+        marginRight: 40
     }
 }));
 
@@ -45,10 +70,15 @@ const useStyles = makeStyles((theme) => ({
 
 function SummonerCard(props) {
     const classes = useStyles();
+
+    const name = props.name;
+    const profile_icon = `http://ddragon.leagueoflegends.com/cdn/10.22.1/img/profileicon/${props.profile_icon}.png`;
+    const level = props.level;
+
     return(
         <div>
             <Card className={classes.card}>
-                <div style={{height: 255, background: 'linear-gradient(to right, #9cecfb, #65c7f7, #0052d4)', marginBottom: -90}}/>
+                <div style={{height: 255, background: 'linear-gradient(to right, #bc4e9c, #f80759)', marginBottom: -90}}/>
                 <div style={{marginBottom: 25}}>
                     <div className="custom-shape-divider-bottom-1604297432">
                         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
@@ -60,11 +90,12 @@ function SummonerCard(props) {
                         </svg>
                     </div>
                 </div>
-                <div>
+                <div className={classes.img_container}>
+                    <div className={classes.img} style={{backgroundImage: `url(${profile_icon})`}}/>
                 </div>
-                <div className={classes.name}><h1 style={{fontFamily: 'Baloo Tammudu 2'}}>Schulerj</h1></div>
-                <div className={classes.name}><h3 style={{fontFamily: 'Baloo Tammudu 2'}}>345</h3></div>
-                <Divider style={{marginTop: 30, marginLeft: 40, marginRight: 40}} variant={'middle'}/>
+                <div className={classes.name}><h2>{name}</h2></div>
+                <div className={classes.name}><h3>{level}</h3></div>
+                <div className={classes.divider}/>
                 <div className={classes.avatar_container}>
                     <Avatar className={classes.avatar}>NA</Avatar>
                 </div>
--- src/pages/user_page.js
+++ src/pages/user_page.js
@@ -12,20 +12,24 @@ import SummonerCard from "../components/user_page/summoner_card";
  * */
 
 function UserPage(props) {
-    // let [summonerInfo, setSummonerInfo] = useState({});
-    // useEffect(() => {
-    //     const api_key = config.API_KEY;
-    //     const api_call = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + props.match.params.name + '?api_key=' + api_key;
-    //     const proxy = `https://cors-anywhere.herokuapp.com/`; // Workaround for Riot API not returning CORS allowed header
-    //     axios.get(`${proxy}${api_call}`).then((res) => {
-    //         console.log(res.data);
-    //         setSummonerInfo(res.data)
-    //     })
-    // }, [props.match.params.name]);
+    let [summonerInfo, setSummonerInfo] = useState({});
+    useEffect(() => {
+        const api_key = config.API_KEY;
+        const api_call = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + props.match.params.name + '?api_key=' + api_key;
+        const proxy = `https://cors-anywhere.herokuapp.com/`; // Workaround for Riot API not returning CORS allowed header
+        axios.get(`${proxy}${api_call}`).then((res) => {
+            console.log(res.data);
+            setSummonerInfo(res.data)
+        })
+    }, [props.match.params.name]);
 
     return(
         <div>
-            <SummonerCard/>
+            <SummonerCard
+                name={summonerInfo.name}
+                profile_icon={summonerInfo.profileIconId}
+                level={summonerInfo.summonerLevel}
+            />
         </div>
     )
 }
