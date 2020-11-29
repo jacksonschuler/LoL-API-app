1304f2ccc78347eb8b4ec09b417514b9a192cfa7
jacksonschuler <jacksonschuler11@gmail.com>
2020-11-29 15:19:45 -0600

    radar chart now takes in the proper damage values

--- src/components/match_page/match_content.js
+++ src/components/match_page/match_content.js
@@ -33,6 +33,54 @@ function getOpponentName(id, participants) {
     return participants[id -1].player.summonerName;
 }
 
+function computeKDA(kills, deaths, assists) {
+    if (deaths === 0) {
+        return ((kills + assists) / 1).toFixed(2)
+    }
+    return ((kills + assists) / deaths).toFixed(2)
+}
+
+function getKP(myKills, myAssists, totalKills){
+    return (((myKills + myAssists)/totalKills) * 100).toFixed(1)
+}
+
+function getTotalKills(match, teamId) {
+    let num_kills = 0;
+    let filtered_match = match.participants.filter(player => player.teamId === teamId);
+    filtered_match.forEach(player => {
+        num_kills += player.stats.kills
+    });
+    return num_kills
+}
+
+function getDamagePercent(myDamage, teamDamage){
+    return (myDamage/teamDamage).toFixed(1)
+}
+
+function computeTeamDamage(match,teamId){
+    let damage_dealt = 0;
+    let filtered_match = match.participants.filter(player => player.teamId === teamId);
+    filtered_match.forEach(player => {
+        damage_dealt += player.stats.totalDamageDealtToChampions
+    });
+    return damage_dealt
+}
+
+/**
+ * feature scale an array between 2-3
+ * @param arr array to scale
+ * @return {[]} the array scaled between 2-3
+ */
+function normalize(arr) {
+    let normalized = [];
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    for (let i = 0; i < arr.length; i++) {
+        normalized[i] = 2 + ((arr[i] - min)*(3-2))/(max-min)
+    }
+    return normalized
+}
+
 
 function MatchContent(props) {
 
@@ -44,19 +92,53 @@ function MatchContent(props) {
 
     let opponentName = getOpponentName(opponentTimeline.participantId, props.match['participantIdentities']);
 
+    let opponentId = opponentTimeline.participantId;
+
     let classes = useStyles();
+    //
+    // console.log(playerId);
+    //
+    // console.log(playerTimeline);
+    //
+    // console.log(opponentTimeline);
+    //
+    // console.log(opponentName);
 
-    console.log(playerId);
+    let KDA_arr = props.match['participants'].map(participant => parseFloat(computeKDA(participant.stats.kills, participant.stats.deaths, participant.stats.assists)));
+    let KP_arr = props.match['participants'].map(participant => parseFloat(getKP(participant.stats.kills, participant.stats.assists, getTotalKills(props.match, participant.teamId))));
+    let championDMG_arr = props.match['participants'].map(participant => participant.stats.totalDamageDealtToChampions);
+    let dmgShare_arr = props.match['participants'].map(participant => parseFloat(getDamagePercent(participant.stats.totalDamageDealtToChampions, computeTeamDamage(props.match, participant.teamId))));
+    let dmgTaken_arr = props.match['participants'].map(participant => participant.stats.totalDamageDealtToChampions);
+    let dmgGold = props.match['participants'].map(participant => parseFloat((participant.stats.totalDamageDealtToChampions/participant.stats.goldEarned).toFixed(5)));
 
-    console.log(playerTimeline);
+    let player_data = [
+        normalize(KP_arr)[playerId -1],
+        normalize(KDA_arr)[playerId -1],
+        normalize(championDMG_arr)[playerId -1],
+        normalize(dmgShare_arr)[playerId -1],
+        normalize(dmgTaken_arr)[playerId -1],
+        normalize(dmgGold)[playerId -1]
+    ];
 
-    console.log(opponentTimeline);
-
-    console.log(opponentName);
+    let opponent_data = [
+        normalize(KP_arr)[opponentId -1],
+        normalize(KDA_arr)[opponentId -1],
+        normalize(championDMG_arr)[opponentId -1],
+        normalize(dmgShare_arr)[opponentId -1],
+        normalize(dmgTaken_arr)[opponentId -1],
+        normalize(dmgGold)[opponentId -1]
+    ];
 
     return(
         <div>
-            <MatchRadarChart/>
+            <MatchRadarChart
+                labels={['Kill Participation (%)', 'KDA', 'Damage to Champions', 'Damage Share (%)', 'Damage Taken', 'Damage per Gold']}
+                //TODO normalize this data
+                player_data={player_data}
+                player_label={props.summonerName}
+                opponent_label={opponentName}
+                opponent_data={opponent_data}
+            />
             Match Page
             <div className={classes.footer}/>
         </div>
--- src/components/match_page/match_radar_chart.js
+++ src/components/match_page/match_radar_chart.js
@@ -1,33 +1,54 @@
 import React from 'react';
 import {Radar} from 'react-chartjs-2'
 
-function MatchRadarChart(){
+function MatchRadarChart(props){
 
+    /*
+        Damage:
+        - kill participation
+        - kda
+        - damage to champs
+        - damage share (%)
+        - damage taken
+        - Damage per gold
 
+        Utility:
+        - vision score
+        - total healing
+        - wards purchased
+        - turret kills
+        - wards placed
+        - crowd control score
+
+        Income
+        - CS/Min
+        - gold spent
+        - gold earned
+    */
     return(
         <div>
             <Radar data={{
-                labels: ['Running', 'Swimming', 'Eating', 'Cycling', 'Dying', 'LOL'],
+                labels: props.labels,
                 datasets: [{
-                    label: 'schulerj',
-                    backgroundColor: 'rgba(255,33,94,0.6)',
+                    label: props.player_label,
+                    backgroundColor: 'rgba(255,34,57,0.6)',
                     pointRadius: 0,
                     lineTension: 0,
-                    data: [20, 10, 29, 14, 25, 20]
+                    data: props.player_data
                 },
                     {
-                        label: 'Another Gamer',
+                        label: props.opponent_label,
                         backgroundColor: 'rgba(0,30,255,0.6)',
                         pointRadius: 0,
                         lineTension: 0,
-                        data: [29, 15, 20, 19, 22, 25]
+                        data: props.opponent_data
                     }]}}
                 options={{
                     scale: {
                         ticks: {
                             display: false,
-                            beginAtZero: true,
-                            suggestedMax: 30,
+                            suggestedMin: 1,
+                            suggestedMax: 3.2,
                         },
                         pointLabels: {
                             fontFamily: 'Quicksand',
